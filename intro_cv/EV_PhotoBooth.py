e8c85d16313131afe9e856b3df33404a48ffb896
Eric Viscione <Ericjviscione@gmail.com>
2025-01-23 22:19:45 -0500

    Finished a version of Hw2

--- intro_cv/EV_PhotoBooth.py
+++ intro_cv/EV_PhotoBooth.py
@@ -11,7 +11,7 @@ class vid_modifiers:
     def __init__(self):
         pass
 
-    def static_modifiers(self, frame, font, timestamp_toggle = True, controls_toggle = True, border_toggle = True, logo_toggle = True):   ##add all the text and needed transforms to the image
+    def static_modifiers(self, frame, font, timestamp_toggle = True, controls_toggle = True, border_toggle = True, add_logo_toggle = True):   ##add all the text and needed transforms to the image
         # cv.line(frame,(0,0),(511,511),(255,0,0),5)
         if timestamp_toggle:
             frame = self.timestamp(frame, font)
@@ -19,7 +19,7 @@ class vid_modifiers:
             frame = self.controls(frame, font)
         if border_toggle:
             frame = self.border(frame)
-        if logo_toggle:
+        if add_logo_toggle:
             frame = self.add_logo(frame, filepath = 'opencv_logo.png')
         return frame
 
@@ -29,7 +29,6 @@ class vid_modifiers:
         for i, text in enumerate(controls):
             cv.putText(frame, text, (30, 30+i*15), font, 0.5, (0,0,0), 2 )
         cv.rectangle(frame,(2,2),(325,75),(255,255,255),-1) 
-
         return frame
 
     def timestamp(self, frame, font):
@@ -37,26 +36,22 @@ class vid_modifiers:
         date_text = f"{current_time.year}/{current_time.month}/{current_time.day}  {current_time.hour}:{current_time.minute}"
         cv.rectangle(frame,(300,480),(640,450),(255,255,255),-1) 
         cv.putText(frame, date_text, (300, 475), font, 1, (0, 0, 0), 2)
-        
-
         return frame
+    
     def border(self, frame, color = [0, 0, 255], width = 10):
         return cv.copyMakeBorder(frame, width, width,width, width, cv.BORDER_CONSTANT, value=color)
-    def add_logo(self, frame, filepath):
-        # print(filepath)
+    
+    def add_logo(self, frame, filepath, opac_1 = 0.8, opac_2 = 0.5):
+        frame_copy = frame
         logo = cv.imread(filepath)       #read in image
-        rows,cols,_ = logo.shape  #get the shape of the logo 
-        roi = frame[0:rows, 0:cols]      #the roi is the size of the logo
-        
-        # Now create a mask of logo and create its inverse mask also
+        assert logo is not None, "file could not be read, check with os.path.exists()"
         mask = self.apply_threshold(logo, 5, 255) #apply the threshold to the mask and return back the logo itself with black everywhere else
-        img2_fg = cv.bitwise_and(logo,logo,mask = mask)
-        mask_inv = cv.bitwise_not(mask)
-        img1_bg = cv.bitwise_and(roi,roi,mask = mask_inv)
-        # frame = cv.bitwise_and(roi,roi,mask = mask_inv)
-        return img1_bg
-        # 
-        # img2_fg = cv.bitwise_and(logo,logo,mask = mask)
+        img2_fg = cv.resize(cv.bitwise_and(logo,logo,mask = mask), (100,100))
+        rows,cols,_ = img2_fg.shape
+        frame[0:rows, 0:cols] = img2_fg
+        blended_frame = cv.addWeighted(frame_copy, opac_1, frame, 1-opac_1, 0)
+        return blended_frame
+
 
     def zoom(self, frame, zoom_factor):
         if zoom_factor < 1: #corrects the zoom factor so we never divide by 0 
@@ -67,16 +62,13 @@ class vid_modifiers:
         w = int(width / (2 * zoom_factor))
         h = int(height / (2 * zoom_factor))
         frame_region = frame[height_center - h:height_center + h, width_center - w:width_center + w]
-
         return cv.resize(frame_region, (width, height))
+    
     def extract_color(self, frame, color_range = []):
         color_range = [np.array([110,50,50]),np.array([130,255,255])]
         hsv_frame = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         mask = cv.inRange(hsv_frame, color_range[0], color_range[1])
         return cv.bitwise_and(frame,frame,mask = mask)
-        # print(color_range[0])
-        # print(F"Holder")
-        # return frame
     
     def rotate_image(self, frame, angle = 10):   #default 10 deg rotation
         rows, cols = frame.shape[:2]
@@ -89,13 +81,25 @@ class vid_modifiers:
         _, new_frame = cv.threshold(grayscale,lower_threshold, upper_threshold, threshold_type)
         return new_frame
     
-    def gaussian_blur(self, frame, x = [], y = []):
-        print(F"Holder")
-        return frame
+    def gaussian_blur(self, frame, kernel = 5, x = None, y = None):
+        if kernel % 2 == 0:
+            kernel += 1  # keep the kernel odd
+        if x == None: x = max(5, min(cv.getTrackbarPos('Gaussian SigmaX', 'EV_Capture'), 30))
+        if y == None: y = max(5, min(cv.getTrackbarPos('Gaussian SigmaY', 'EV_Capture'), 30))
+        blur = cv.GaussianBlur(frame, (kernel,kernel), x, y)
+        return blur
     
-    def sharpen_image(self, frame):
-        print(F"Holder")
-        return frame
+    def sharpen_image(self, frame, alpha = 0.75):
+        alpha = cv.getTrackbarPos('Sharpening Alpha', 'EV_Capture')/20
+        kernel = cv.getTrackbarPos('Kernel Size', 'EV_Capture')
+        blurred_image = self.gaussian_blur(frame, kernel)
+        detail = cv.subtract(frame, blurred_image)
+        detail = alpha*detail
+        detail = detail.astype(frame.dtype)
+        # print(F"Frame Type {frame.dtype}, Detail Type {detail.dtype}")
+        new_frame = cv.add(frame, detail)
+        return new_frame
+    
     def copy_roi(self, frame):
         roi = frame[455:485,300:620] #rows, cols
         frame[5:35,320:640] = roi
@@ -119,6 +123,9 @@ class vid_capture:
         self.rotate = False
         self.threshold = False
         self.copy_roi = False
+        self.gaussian_blur = False
+        self.sharpen = False
+
     def create_directory(self):
         try: ##directory creation references from geeksforgeeks.com
             os.mkdir(self.directory)
@@ -130,7 +137,6 @@ class vid_capture:
         except Exception as e:
             print(f"An error occurred: {e}")
 
-
     def save_image(self,frame, color = []):
         print("Captured Image")
         # rows, cols, channels = frame.shape
@@ -139,7 +145,6 @@ class vid_capture:
         frame[:] = [255, 255, 255] #flash white
         print(f"Frames shape is{frame.shape}")
         return frame
-
 
     def toggle_capture(self):
         """Start or stop video recording."""
@@ -157,12 +162,9 @@ class vid_capture:
     def toggle_action(self,flag, action = []):
         print(F"{action}")
         if flag:
-            
             print(f"Starting {action}:")
         else:
             print(f"Stopping {action}.")
-
-        # self.extract = not self.extract
 
     def generate_name(self, prefix, file_type): #genereates the name for our images and captures
         filename = f"{prefix}-{datetime.datetime.now():%Y-%m-%d_%H-%M-%S}.{file_type}"
@@ -172,6 +174,10 @@ class vid_capture:
         self.create_directory()
         cv.namedWindow('EV_Capture')
         cv.createTrackbar('Zoom','EV_Capture',1,255,lambda x: None)
+        cv.createTrackbar('Gaussian SigmaX','EV_Capture',5,30,lambda x: None)
+        cv.createTrackbar('Gaussian SigmaY','EV_Capture',5,30,lambda x: None)
+        cv.createTrackbar('Sharpening Alpha','EV_Capture',0,100,lambda x: None)
+        cv.createTrackbar('Kernel','EV_Capture',0,20,lambda x: None)
 
     def actions(self, frame, action, color = []):
             if action == ord('c'):
@@ -189,13 +195,14 @@ class vid_capture:
                 self.threshold = not self.threshold
                 self.toggle_action(self.threshold, "Applying Threshold")
             elif action == ord('b'):
-                print(F"Gaussian Blurring")
+                self.gaussian_blur = not self.gaussian_blur
+                self.toggle_action(self.gaussian_blur, "Gaussian Blurring")
             elif action == ord('s'):
-                print(F"Sharpen")
+                self.sharpen = not self.sharpen
+                self.toggle_action(self.sharpen, "Sharpening")
             elif action == ord('m'):
                 self.copy_roi = not self.copy_roi
                 self.toggle_action(self.copy_roi, "Copying ROI")
-
             elif action == 27: #escape key
                 self.running = False
             return frame
@@ -223,6 +230,10 @@ class vid_capture:
                 frame = self.modifiers.apply_threshold(frame) #optional arguments for the lower and upper threshold bounds adn threshold type eg apply_threshold(frame, lwoer, upper, type)
             if self.copy_roi:
                 frame = self.modifiers.copy_roi(frame)
+            if self.gaussian_blur:
+                frame = self.modifiers.gaussian_blur(frame)
+            if self.sharpen:
+                frame = self.modifiers.sharpen_image(frame)
             cv.imshow('frame', frame)
             if self.capture_video and self.out:
                 self.out.write(frame)
